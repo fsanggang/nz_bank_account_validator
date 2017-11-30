67c805e129715e8df90a69bb63bc5ccd33001ee0
Fiona Sanggang <fsanggang@gmail.com>
2017-11-30 20:33:27 +1300

    Initial commit

---
+++ lib/nz_bank_account_validator.ex
@@ -0,0 +1,18 @@
+defmodule NzBankAccountValidator do
+  @moduledoc """
+  Documentation for NzBankAccountValidator.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NzBankAccountValidator.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nz_bank_account_validator_test.exs
@@ -0,0 +1,8 @@
+defmodule NzBankAccountValidatorTest do
+  use ExUnit.Case
+  doctest NzBankAccountValidator
+
+  test "greets the world" do
+    assert NzBankAccountValidator.hello() == :world
+  end
+end
