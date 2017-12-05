f149fed6313895929dec6c7be8cd5d384ef29225
Fiona Sanggang <fsanggang@gmail.com>
2017-12-06 04:28:15 +1300

    Prep to publish to hex

--- mix.exs
+++ mix.exs
@@ -1,17 +1,22 @@
 defmodule NzBankAccountValidator.Mixfile do
   use Mix.Project
 
+  @github_url "https://github.com/fsanggang/nz_bank_account_validator"
+
   def project do
     [
       app: :nz_bank_account_validator,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.5",
+      name: "NzBankAccountValidator",
+      description: "Validates NZ bank account numbers",
       start_permanent: Mix.env == :prod,
       deps: deps(),
+      package: package(),
 
       # Docs
       name: "NzBankAccountValidator",
-      source_url: "https://github.com/fsanggang/nz_bank_account_validator"
+      source_url: @github_url
     ]
   end
 
@@ -28,6 +33,14 @@ defmodule NzBankAccountValidator.Mixfile do
       {:ex_doc, "~> 0.16", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: [ "Fiona Sanggang" ],
+      licenses: [ "MIT" ],
+      links: %{ "GitHub" => @github_url }
     ]
   end
 end
